39979a02202ba7304656367cff15522e869a8218
7bvcxz <7bvcxz@gmail.com>
2022-11-23 19:05:07 +0900

    Lstm_BLAS Ended

---
+++ how.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Go to "https://www.intel.com/content/www/us/en/developer/tools/oneapi/onemkl-link-line-advisor.html"
+# Add Compile Options to Makefile
+# Make
+
+make clean && make && ./lstm
